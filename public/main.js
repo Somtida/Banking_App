0f629e0a6d996c8d48e05f6ac3c749e73d44425e
S.SKNS <somtida.th@gmail.com>
2016-06-22 21:54:10 -0700

    local

--- public/main.js
+++ public/main.js
@@ -2,26 +2,38 @@
 
 let app = angular.module('myApp', ['ngStorage'])
 
-app.controller('mainCtrl', function($scope, $localStorage,$sessionStorage){
-  $scope.transactions = [];
+app.controller('mainCtrl', function($scope, $localStorage){
+  //delete $localStorage.balance;
+  //delete $localStorage.transactions;
+  $scope.transactions = $localStorage.$default({
+    balance: 0,
+    transactions: []
+  });
 
-  $scope.deleteIt = (index) => {
-    console.log("index: ",index);
-    $scope.transactions.splice(index, 1);
-  }
+  console.log("$scope.transactions.transactions", $scope.transactions.transactions);
+
 
   $scope.addTransaction = () => {
     $scope.newTransaction.date = new Date();
-    // $scope.newTransaction.type = $scope.newTransaction.type || debit;
+    if($scope.newTransaction.type === "debit"){
+      $scope.transactions.balance -= parseInt($scope.newTransaction.amount);
+    }else{
+      $scope.transactions.balance += parseInt($scope.newTransaction.amount);
+    }
     let newTransaction = angular.copy($scope.newTransaction);
-    $scope.transactions.push($scope.newTransaction);
-
-    $scope.newTransaction = null;
-
-
+    $scope.transactions.transactions.push(newTransaction);
 
   }
 
 
+  $scope.deleteIt = (index) => {
+    console.log("index: ",index);
+    $scope.transactions.transactions.splice(index, 1);
+  }
 
+  // $scope.totalBalance = () => {
+  //   let $scope.total = $scope.data.transactions.map(tran => {
+  //     console.log("tran: ",tran);
+  //   });
+  // }
 });
